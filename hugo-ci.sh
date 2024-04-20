aa3c54c059ee38c9a3a435818f12658daa5e0e0a
Nguyen Thanh Nguyen (Andy) <ntnguyencse@gmail.com>
2024-04-20 13:01:17 +0900

    Update hugo-ci.sh

--- hugo-ci.sh
+++ hugo-ci.sh
@@ -6,6 +6,7 @@ export DART_SASS_VERSION=1.63.5
 curl -LJO https://github.com/sass/dart-sass/releases/download/${DART_SASS_VERSION}/dart-sass-${DART_SASS_VERSION}-linux-x64.tar.gz && \
 tar -xf dart-sass-${DART_SASS_VERSION}-linux-x64.tar.gz && \
 rm dart-sass-${DART_SASS_VERSION}-linux-x64.tar.gz && \
+ls -lash && \
 export PATH=/opt/build/repo/dart-sass:$PATH && \
 sass --version && \
 hugo version && \
